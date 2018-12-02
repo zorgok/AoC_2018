2880e3fa61c5979af2183dc004b5938e901ef947
zorgok <44413312+zorgok@users.noreply.github.com>
2018-12-02 18:14:59 +0100

    Add files via upload

---
+++ Day 1/Day_1_first_part.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int i, j=0, p, sum=0;
+    FILE *text;
+    text = fopen("Input_day_1.txt","r");
+    if(!text)
+        return 1;
+    while(1){
+
+        fscanf(text, "%d", &i);
+        if(feof(text))
+        break;
+        sum+= i;
+            }
+    printf("Resulting frequency is : %d",sum);
+    fclose(text);
+    return 0;
+}
